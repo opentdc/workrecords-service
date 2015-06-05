1e721fe0d2992bf4b3055d9873abfbb1aedbf499
Bruno Kaiser <bruno@bkaiser.ch>
2015-06-05 20:07:25 +0200

    added WorkRecordComparator (based on id)

--- src/java/org/opentdc/workrecords/WorkRecordModel.java
+++ src/java/org/opentdc/workrecords/WorkRecordModel.java
@@ -23,6 +23,7 @@
  */
 package org.opentdc.workrecords;
 
+import java.util.Comparator;
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -32,7 +33,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
 public class WorkRecordModel {
-	private String id;
+	private String id;				// sortable
 	private String projectId;
 	private String resourceId;
 	private Date startAt;
@@ -226,4 +227,26 @@ public class WorkRecordModel {
 	public void setModifiedBy(String modifiedBy) {
 		this.modifiedBy = modifiedBy;
 	}
+	
+	/******************************* Comparator *****************************/
+	public static Comparator<WorkRecordModel> WorkRecordComparator = new Comparator<WorkRecordModel>() {
+
+		public int compare(WorkRecordModel obj1, WorkRecordModel obj2) {
+			if (obj1.getId() == null) {
+				return -1;
+			}
+			if (obj2.getId() == null) {
+				return 1;
+			}
+
+			String _attr1 = obj1.getId();
+			String _attr2 = obj2.getId();
+
+			// ascending order
+			return _attr1.compareTo(_attr2);
+
+			// descending order
+			// return _attr2.compareTo(_attr1);
+		}
+	};
 }
